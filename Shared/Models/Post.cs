a90ffd9657cdee36680a07d605895421d6ebc14c
igorcretu <64871246+igorcretu@users.noreply.github.com>
2023-10-31 23:03:31 +0100

    model/post

---
+++ Shared/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿namespace Shared.Models;
+
+public class Post
+{
+    public int Id { get; set; }
+    public User Owner { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
+    
+    public Post(User owner, string title, string body)
+    {
+        Owner = owner;
+        Title = title;
+        Body = body;
+    }
+}
